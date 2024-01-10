e727991297b5d3ded715a554b0cdedb397f3e7d5
Jon Roberts <jgrcloud@amazon.com>
2024-01-10 11:22:23 -0600

    add load-tpc-ds

--- load-tpc-ds/tpcds_variables.sh
+++ load-tpc-ds/tpcds_variables.sh
@@ -3,6 +3,6 @@ SCHEMA_NAME="tpcds"
 EXPLAIN="false"
 MULTI_USER_COUNT="5"
 #Available scale sizes: 10GB, 100GB, 1TB, 3TB, 10TB, 30TB, 100TB
-SCALE="10GB"
+SCALE="3TB"
 SOURCE_BUCKET="redshift-downloads/TPC-DS/2.13"
 TEMP_BUCKET="jgrcloud-temp"
